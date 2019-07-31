5645b3ff9410caab65cecbd132e726bdfec85502
omar1413 <omarosama200100@gmail.com>
2019-07-31 02:39:06 +0200

    add Bank  model

---
+++ lib/src/models/bank.dart
@@ -0,0 +1,14 @@
+class Bank {
+  String name;
+  String logo;
+  double dollarBuyPrice;
+  double dollarSellPrice;
+  String dollarMovement;
+
+  Bank(
+      {this.name,
+      this.logo,
+      this.dollarBuyPrice,
+      this.dollarSellPrice,
+      this.dollarMovement});
+}
